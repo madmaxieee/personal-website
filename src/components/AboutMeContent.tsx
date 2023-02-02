fc39d04a40059897b694776a8854283f2cdf2207
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-02-03 03:42:22 +0800

    update links

--- src/components/AboutMeContent.tsx
+++ src/components/AboutMeContent.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
 
 import { Text, Container } from "@mantine/core";
-import { IconBrandGithub, IconMail } from "@tabler/icons-react";
+import {
+  IconMail,
+  IconDownload,
+  IconBrandGithub,
+  IconBrandLinkedin,
+} from "@tabler/icons-react";
 
 import avatar from "@/assets/images/avatar.png";
 
@@ -10,7 +15,7 @@ import LinkButton from "@/components/LinkButton";
 export const AboutMeContent = () => {
   return (
     <Container size="sm" className="ml-144 pt-16">
-      <div className="mb-8 flex items-center gap-8">
+      <div className="mb-8 flex items-center gap-6">
         <Image
           src={avatar}
           width={200}
@@ -33,15 +38,30 @@ export const AboutMeContent = () => {
           >
             Visit GitHub
           </LinkButton>
+          <LinkButton
+            href="www.linkedin.com/in/chuangjiaxu"
+            color="red"
+            icon={<IconBrandLinkedin />}
+          >
+            Visit LinkedIn
+          </LinkButton>
+          <LinkButton
+            href="/files/Max_Chuang_Resume.pdf"
+            color="red"
+            icon={<IconDownload />}
+          >
+            Download Resume
+          </LinkButton>
         </div>
       </div>
       <article>
         <Text className="text-2xl font-semibold">
-          I major in electrical engineering at National Taiwan University. I'm a
-          software engineer who loves to create things. I can build applications
-          in teams. I work as a freelance engineer for businesses as well. I can
-          speak English well. I'm excited to pursue a career as a software
-          engineer, whether it be here in Taiwan or abroad.
+          My name is 莊加旭, or Max Chuang. I major in electrical engineering at
+          National Taiwan University. I'm a software engineer who loves to
+          create things. I can build applications in teams. I work as a
+          freelance engineer for businesses as well. I can speak English well.
+          I'm excited to pursue a career as a software engineer, whether it be
+          here in Taiwan or abroad.
         </Text>
       </article>
     </Container>
---
+++ src/components/ScrollProgress.tsx
@@ -0,0 +1,24 @@
+import { useSpring, animated } from "@react-spring/web";
+
+export interface ScrollProgressProps {
+  progress: number;
+}
+
+export const ScrollProgress = ({ progress }: ScrollProgressProps) => {
+  const props = useSpring({
+    progress,
+  });
+
+  return (
+    <div className="sticky top-0 z-10 h-2 w-full bg-red-900">
+      <animated.div
+        className="sticky top-0 z-10 h-2 w-full bg-red-600"
+        style={{
+          width: props.progress.to((x) => `${x * 100}%`),
+        }}
+      />
+    </div>
+  );
+};
+
+export default ScrollProgress;
